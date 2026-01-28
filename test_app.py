8a3cc0987befbf6f3085f45dde78b7fe9b61ee64
Himanshu Mishra <himanshu.mishra3@publicissapient.com>
2026-01-28 15:17:29 +0530

    k

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from app import app
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.data == b"Hello CI"
+
+def test_health():
+    client = app.test_client()
+    response = client.get("/health")
+    assert response.data == b"OK"
